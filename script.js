a659699ecfdba9d05721a955ed4dbe25af382a64
Ruth Gicheha <raiynegicheha@gmail.com>
2023-08-18 02:00:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const createBtn = document.querySelector(".btn");
 
 let notes = document.querySelectorAll(".input-box");
 
+function showNodes (){
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNodes ();
+
+function updateStorage(){
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
 createBtn.addEventListener("click", ()=>{
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
@@ -15,5 +24,13 @@ createBtn.addEventListener("click", ()=>{
 notesContainer.addEventListener("click", function(e){
     if(e.target.tagName === "IMG"){
         e.target.parentElement.remove();
+        updateStorage()
+    } else if(e.target.tagName === 'P'){
+        notes =  document.querySelectorAll(".input-box");
+        notes.forEach(nt =>{
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
     }
 })
